68c8489fbdb9ffeed777b182fcc79d6c5a1aaf2f
Noah Brewer <Noah.T.Brewer@gmail.com>
2023-01-05 22:38:11 -0600

    wip

--- skelly.js
+++ skelly.js
@@ -1,16 +1,42 @@
+function wrap(el, wrapper, class_name="", class_id="") {
+    if(class_name.length != 0){
+        wrapper.classList.add(class_name)
+    }
+    if(class_id.length != 0){
+        wrapper.classList.add(class_name + class_id)
+    }
+    el.parentNode.insertBefore(wrapper, el);
+    wrapper.appendChild(el);
+}
+
+// example: wrapping an anchor with class "wrap_me" into a new div element
+document.querySelectorAll('img.image').forEach( (elem, id) => {
+    wrap(elem, document.createElement('div'), `picBackground`, `${id}`);
+})
+
 let options = {
     rootMargin: '0px',
     threshold: .3
   }
-window.onbeforeunload = function () {
+
+/*window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-}
+}*/
+setTimeout(function () {
+    window.scrollTo(0, 0);
+},2);
 
 let pageIntersectionElems = []
 pageIntersectionElems.push(document.querySelector('.about'))
+pageIntersectionElems.push(document.querySelector('.aboutSupport'))
 pageIntersectionElems.push(document.querySelector('.skills'))
+pageIntersectionElems.push(document.querySelector('.skillsSupport'))
 pageIntersectionElems.push(document.querySelector('.work'))
+pageIntersectionElems.push(document.querySelector('.workSupport'))
+pageIntersectionElems.push(document.querySelector('.projects'))
 pageIntersectionElems.push(document.querySelector('.contact'))
+
+
 
 
 
